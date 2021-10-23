61af3b548e5c6c0f83acb99143c504ed976c7abf
aadne <fmailscammer@gmail.com>
2021-10-23 15:29:04 -0400

    Move Navbar to its own component

--- src/components/buttons/authButton.js
+++ src/components/buttons/authButton.js
@@ -11,7 +11,9 @@ const AuthButton = () => {
       <div className="flex">
         <a href="/" className="flex mx-2">
           <img
-            className="transition-all w-16 h-16 my-auto rounded-full hover:rounded-xl border-black border-2"
+            className="transition-all
+                        transform w-16 h-16 moverflow-hidden
+                        my-auto rounded-full scale-100 hover:scale-90"
             src={user.picture}
             alt="user icon"
           />{" "}
